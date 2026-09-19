fc7913dfc9a63a341acec8c459e6e943dc5e6190
Rehan <rehansiddiqui0653@gmail.com>
2026-09-19 20:04:24 +0530

    LoggerV1 completed

--- LoggerV1.py
+++ LoggerV1.py
@@ -2,21 +2,19 @@
 
 #imports
 import datetime
-
-#Possible points according to day 
-
-def possiblepoints():
-    day = datetime.datetime.now().weekday()
-    if day == 1 or day == 3:
-        possible_points = 7
-    if day == 6:
-        possible_points = 6
-    else:
-        possible_points = 8
-    return possible_points
+import csv
 
 #Daily Goals
 def entry():
+    global wakeupmsg
+    global phonedowntime
+    global move
+    global guitar
+    global leisuretime
+    global commit
+    global Problems
+    global Logtime
+
     wakeupmsg = input("Woke up by 06:00 & sent message? [Y/N]: ")
     phonedowntime = input("Phone in downtime from 16:00 to 20:45 & 21:45? [Y/N]: ")
     move = input("Walk/Exercise/Cycle/Sport done? [Y/N]: ")
@@ -26,8 +24,25 @@ def entry():
     Problems = input("Problems solved? [Enter number of problems]: ")
     Logtime = input("Log updated and sent by 20:45? [Y/N]: ")
 
+
+#Possible points according to day 
+
+def possiblepoints():
+    global possible_points
+    possible_points = 0
+
+    day = datetime.datetime.now().weekday()
+    if day == 1 or day == 3:
+        possible_points = 7
+    if day == 6:
+        possible_points = 6
+    else:
+        possible_points = 8
+    return possible_points
+
 #Points Achieved (Daily)
-def dayscore():
+def obtainedpoints():
+    global points
     points = 0
     
     if wakeupmsg.lower() == 'y':
@@ -47,3 +62,68 @@ def dayscore():
     if Logtime.lower() == 'y':
         points += 1
     return points
+
+#Day's score in percentage
+
+def dayscore():
+    global score
+    score = (points / possible_points) * 100
+    return score
+
+#Call functions   
+entry()
+possiblepoints()
+obtainedpoints()
+dayscore()
+
+
+#Store data to a CSV file
+
+def updatecsv():
+    f = open('LoggerV1.csv', 'a')
+    #header = ['Day', 'Date', 'Possible Points', 'Points Obtained', 'Day %', 'Day Color']
+    dayno = datetime.datetime.now().weekday()
+    day = ''
+    if dayno == 0:
+        day = "Monday"
+    elif dayno == 1:
+        day = "Tuesday"
+    elif dayno == 2:
+        day = "Wednesday"
+    elif dayno == 3:
+        day = "Thursday"
+    elif dayno == 4:
+        day = "Friday"
+    elif dayno == 5:
+        day = "Saturday"
+    else:
+        day = "Sunday"
+    daycolor = ''
+    if score >= 85:
+        daycolor = 'green'
+    elif score >= 70:
+        daycolor = 'amber'
+    else:
+        daycolor = 'red'
+    date = datetime.date.today()
+    record = [day, date, possible_points, points, score, daycolor]
+    writer = csv.writer(f)
+    writer.writerow(record)
+    f.close()
+
+#Output generation
+daycolor = ''
+if score >= 85:
+    daycolor = 'green'
+elif score >= 70:
+    daycolor = 'amber'
+else:
+    daycolor = 'red'
+print(f"Possible points: {possible_points}")
+print(f"Points obtained: {points}")
+print(f"Today's score: {score}% ({daycolor} day)")
+update = input("Update log? [Y/N]: ")
+if update.lower() == 'y':
+    updatecsv()
+else:
+    pass
