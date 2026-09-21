ed7fada80cd45c8cd536c7afeda639afe9709918
Rehan <rehansiddiqui0653@gmail.com>
2026-09-21 20:21:17 +0530

    tkinter test

--- test area/test.py
+++ test area/test.py
@@ -1,3 +1,8 @@
-import datetime
-day = datetime.datetime.now().weekday()
-print(day)
+import tkinter as tk
+root = tk.Tk()
+root.title("Test GUI")
+label = tk.Label(root, text="Hello creature with eyes.", width=100, height=100, bg="lightblue", fg="red", font=("Arial", 100), borderwidth=2)
+label.pack()
+button = tk.Button(root, text="Click me", command=lambda: label.config(text="You clicked the button!"))
+button.pack()
+root.mainloop()
